d88c80c7c38ea39048fbaffd92250498d81f7242
jessicahattig <jessicahattig@gmail.com>
2023-03-24 11:58:34 -0700

    Changing form questions.

--- scripts.js
+++ scripts.js
@@ -7,6 +7,14 @@ function subtract(num1, num2) {
   return num1 - num2;
 }
 
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
 // User Interface Logic
 function handleCalculation(event) {
   event.preventDefault();
@@ -19,10 +27,13 @@ function handleCalculation(event) {
     result = add(number1, number2);
   } else if (operator === "subtract") {
     result = subtract(number1, number2);
-  } 
+  } else if (operator === "multiply") {
+    result = multiply(number1, number2);
+  } else if (operator === "divide") {
+    result = divide(number1, number2);
+  }
 
-  document.getElementById("output").innerText = {
-    result;
+  document.getElementById("output").innerText = result;
 }
 
 window.addEventListener("load", function() {
