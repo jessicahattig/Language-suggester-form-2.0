e964180721e83165eaf4cef769db6d50c1153697
jessicahattig <jessicahattig@gmail.com>
2023-03-29 06:56:22 -0700

    adding form output on html and js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 window.onload = function() {
+  //event handler for form submission
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
+    //printing output
     event.preventDefault();
     const someInput = document.getElementById("some-input").value;
   };
