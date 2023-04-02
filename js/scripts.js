b097c6492c00c4a7153005882c76b4fea46bdcf3
jessicahattig <jessicahattig@gmail.com>
2023-04-02 13:00:21 -0700

    Checking to see if input from first question is pulled. Confirmed it worked.

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-var form = document.getElementById('form')
-
-form.addEventListener('submit', function(event){
-  event.preventDefault()}
-  
-  var firstQ = document.getElementById(first-input).
-
-  console.log(firstQ)
