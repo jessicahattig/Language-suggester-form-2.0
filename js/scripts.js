310870c05d1cc9d81478e8279522692ca998ded6
jessicahattig <jessicahattig@gmail.com>
2023-04-02 11:57:03 -0700

    WIP: forth question.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var form = document.getElementById('form')
+
+form.addEventListener('submit', function(event){
+  event.preventDefault()}
+  
+  var firstQ = document.getElementById(first-input).
+
+  console.log(firstQ)
