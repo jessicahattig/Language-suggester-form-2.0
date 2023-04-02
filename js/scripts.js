88f91e4d8874032a77b5f71f77c649c66fc3e78b
jessicahattig <jessicahattig@gmail.com>
2023-04-02 11:36:01 -0700

    WIP fixing forth question.

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-  //event handler for form submission
-  let form = document.querySelector("form");
-  form.onsubmit = function(event) {
-    //printing output
-    event.preventDefault();
-    const someInput = document.getElementById("some-input").value;
-  };
-};
