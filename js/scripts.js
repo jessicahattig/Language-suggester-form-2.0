81d69582e0f60439b440a392598f1d4e80299f68
jessicahattig <jessicahattig@gmail.com>
2023-04-02 15:41:34 -0700

    WIP: outputting results from survey.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 function surveyResults(){
-  alert("Testing")
+  let firstInput = document.querySelector("#firstInput");
+  let secondInput = document.querySelector("#secondInput");
+  let thirdInput = document.querySelector("#thirdInput");
+  let forthInput = document.querySelector("#forthInput");
+  let fifthInput = document.querySelector("#fifthInput");
+  let results = documentt.querySelector("#surveyResults");
+
+  surveyResults.innerHTML = "The programming language you should learn is...." 
 }
