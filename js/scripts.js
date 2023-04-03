421c40e1bb1b3d3b127814cc3baafb5d491d3517
jessicahattig <jessicahattig@gmail.com>
2023-04-02 20:45:23 -0700

    WIP: Hiding sentence the goes with result.

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,29 @@ function result(event) {
   let finalResult = document.getElementById("finalResult"); 
   finalResult.removeAttribute("class")
   event.preventDefault();
-} 
+//branching logic for results, boolean style
+let result;
+if (firstAnswer === "Yes"){
+  result= Ruby;
+} else if (secondAnswer==="Yes"){
+  result = Python;
+} else if (thirdAnswer==="Yes"){
+  result= JavaScript;
+} else if (fourthAnswer==="Yes"){
+  result=Ruby;
+} else if (fifthAnswer==="Yes"){
+  result = Ruby;
+} else if (firstAnswer==="No"){
+  result=Python;
+} else if (secondAnswer==="No"){
+  result = JavaScript;
+} else if (thirdAnswer==="No"){
+  result= Python;
+} else if (fourthAnswer==="No"){
+  result = Javascript;
+} else if (fifthAnswer==="No"){
+  result=JavaScript;
+}
+
+document.getElementById("finalResult").innerText = " ";
+}
