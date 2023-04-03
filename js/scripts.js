d0dfaffc013d57512a1e600b8339ee645c048814
jessicahattig <jessicahattig@gmail.com>
2023-04-02 20:04:11 -0700

    Reworing JS logic to make more sense.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,14 @@
-window.onload = function() {
-  console.log("Script executing!");
-  // Event handler for the form submissions
-  let languageForm = document.querySelector("form");
-  form.onsubmit = function(event) {
-    event.preventDefault();
-    console.log("submitting form successfully");
+// Variables - languages
+const JavaScript = "JavaScript!"
+const Python = "Python!"
+const Ruby = "Ruby!"
 
-  // Variables
-  
-
-  // Getting the value from each form input.
-    const firstInput = document.getElementById("firstInput").value;
-    const secondInput = document.getElementById("secondInput").value;
-    const thirdInput = document.getElementById("thirdInput").value;
-    const fourthInput = document.getElementById("fourthInput").value;
-    const fifthInput = document.getElementById("fifthInput").value;
+// Getting the value from each question input.
+const firstInput = document.getElementById("firstInput").value;
+const secondInput = document.getElementById("secondInput").value;
+const thirdInput = document.getElementById("thirdInput").value;
+const fourthInput = document.getElementById("fourthInput").value;
+const fifthInput = document.getElementById("fifthInput").value;
   };
 };
 
