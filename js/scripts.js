acc4ec9886b426b0eb3dff1847d6423ac9de1d50
jessicahattig <jessicahattig@gmail.com>
2023-04-02 18:36:49 -0700

    WIP: showing survey results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 window.onload = function() {
+  console.log("Script executing!");
   // Event handler for the form submissions
   let languageForm = document.querySelector("languageForm");
-  languageForm.onsubmit = function(e) {
-    e.preventDefault();
-  }
-  // Getting the value from each form input.
-  //
-  const firstInput = document.getElementById("firstInput").value;
-  const secondInput = document.getElementById("secondInput").value;
-  const thirdInput = document.getElementById("thirdInput").value;
-  const forthInput = document.getElementById("fourthInput").value;
-  const fifthInput = document.getElementById("fifthInput").value;
+  languageForm.onsubmit = function(event) {
+    event.preventDefault();
+    
 
+  // Getting the value from each form input.
+    const firstInput = document.getElementById("firstInput").value;
+    const secondInput = document.getElementById("secondInput").value;
+    const thirdInput = document.getElementById("thirdInput").value;
+    const forthInput = document.getElementById("fourthInput").value;
+    const fifthInput = document.getElementById("fifthInput").value;
+
+  };
+};
+
+  // Hidding results before form is submitted
+  
