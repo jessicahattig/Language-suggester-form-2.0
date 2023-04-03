1ae605ff729bf9e39eefad6deb43aa4954ca8ca7
jessicahattig <jessicahattig@gmail.com>
2023-04-02 18:57:46 -0700

    Simplified form questions to be yes or no.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 window.onload = function() {
   console.log("Script executing!");
   // Event handler for the form submissions
-  let languageForm = document.querySelector("languageForm");
-  languageForm.onsubmit = function(event) {
+  let languageForm = document.querySelector("form");
+  form.onsubmit = function(event) {
     event.preventDefault();
-    
+    console.log("submitting form successfully");
 
   // Getting the value from each form input.
     const firstInput = document.getElementById("firstInput").value;
@@ -13,6 +13,8 @@ window.onload = function() {
     const forthInput = document.getElementById("fourthInput").value;
     const fifthInput = document.getElementById("fifthInput").value;
 
+    function surveyResults =
+    
   };
 };
 
